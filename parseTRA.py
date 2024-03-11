c3233894f1198937f11dfef618d0e27d7b6fae80
Simone Olubo <oluboso@ai-hpcn112.niaid.nih.gov>
2024-03-11 14:04:04 -0400

    add parseTRA.py that handles overlapping TRA/TRD alleles

---
+++ parseTRA.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+from pybedtools import BedTool
+import csv, re
+import pandas as pd
+
+def parseTRA(gffFile):
+	#open the TRD annotations output file
+	with open(f"annoTemp/TRDalleles.gff", 'w') as output:
+		writer = csv.writer(output, delimiter="\t")
+		with open(gffFile, 'r') as input:
+			reader = csv.reader(input, delimiter="\t")
+			for row in reader:
+				#write row with TRD annotations to TRDalleles.gff
+				if re.match("TRD",row[2]):
+					writer.writerow(row)
+	TRDalleles = BedTool("annoTemp/TRDalleles.gff").sort()
+	allAlleles = BedTool(gffFile).sort()
+
+	#use BedTool to cluster the alleles
+	BedTool(allAlleles).cluster(s=True).saveas(f"annoTemp/clusteredAlleles.gff")
+
+	columnNames =["contig","source","feature","start","end","score","strand","frame","alleleName","cluster"]
+	df = pd.read_csv("annoTemp/clusteredAlleles.gff",delimiter="\t",error_bad_lines=False,header=None)
+	df.columns = columnNames
+
+	grouped = df.groupby('cluster')
+	df2 = pd.DataFrame()
+	#create window from functional TRDV to TRDC, and remove fake TRA psuedogenes within window and fake TRD pseudos outside!
+	for group_name, group_data in grouped:
+		#find if there are any TRA and TRD genes within the same group
+		traFound = any(group_data['alleleName'].apply(lambda x: bool(re.search(r'TRA', x))))
+		trdFound = any(group_data['alleleName'].apply(lambda x: bool(re.search(r'TRD', x))))
+		trdPseudoFound = False
+		traPseudoFound = False
+		trdFunctionalFound = False
+		traFunctionalFound = False
+		#look for specific gene features
+		for feature in group_data['feature']:
+			if re.search('TRD_V_pseudogene', feature):
+				trdPseudoFound = True
+			elif re.search('TRA_V_pseudogene', feature):
+				traPseudoFound = True
+			elif re.search('TRA_V_gene', feature):
+				traFunctionalFound = True
+			elif re.search('TRD_V_gene', feature):
+				trdFunctionalFound = True
+		#if both TRA and TRD alleles are found within the same group, determine whether to remove or keep any TRA do TRD functional/pseudogenes
+		if traFound and trdFound:
+			if traFunctionalFound and trdFunctionalFound:
+				print("both TRA and TRD functional genes found, remove TRD")
+				group_data = group_data[~group_data['alleleName'].str.contains('TRD')]
+				df2 = pd.concat([df2,group_data])
+			if traPseudoFound and trdPseudoFound:
+				print("both TRA and TRD pseudogenes found, manually inspect these alleles:")
+				print(group_data[['feature','start','end','alleleName']])
+				df2 = pd.concat([df2,group_data])
+			if traFunctionalFound and trdPseudoFound:
+				group_data = group_data[~group_data['alleleName'].str.contains('TRD')]
+				df2 = pd.concat([df2,group_data])
+			if trdFunctionalFound and traPseudoFound:
+				group_data = group_data[~group_data['alleleName'].str.contains('TRA')]
+				df2 = pd.concat([df2,group_data])
+		else:
+			df2 = pd.concat([df2,group_data])
+
+
+	df2 = df2.drop('cluster',axis=1)
+	contigName = df.loc[1,'contig']
+	
+	#sort and return the final annotations
+	df2.to_csv(f"annoTemp/traTempAnnotations.gff", sep='\t', index=False, header=False)
+	traFinalAnnotations = BedTool("annoTemp/traTempAnnotations.gff").sort()
+	return traFinalAnnotations
