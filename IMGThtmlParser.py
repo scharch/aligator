2ec2b1a60822de6280e6cf2e28bad1ad16fae448
Simone Olubo <simone.olubo@gmail.com>
2024-05-23 20:19:27 -0400

    Changes to IMGT parsing script: fixed C gene bug and also more pseudogene filtering

--- IMGThtmlParser.py
+++ IMGThtmlParser.py
@@ -13,13 +13,16 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 # Initialize variables and lists for tracking gene features
 inGeneVDJ = False
-inGeneC = False
+inGeneCunit = False
 inVgene = False
+inJgene = False
+inCgene = False
 geneName = ""
 start = ""
 end = ""
 geneType = ""
 geneNameList = []
+CexonList = []
 pseudoList = []
 RSList = []
 VSlist = []
@@ -32,13 +35,14 @@ for x in soup.select('div',attrs={'class':'features'}):
 		row = [re.sub("\n",":",i.text.strip()) for i in tr]
 		
 		#check for functional and pseudo V-genes
-		if inVgene and geneType == "V-GENE":
+		if inVgene or inJgene or inCgene and geneType in ["V-GENE", "J-GENE","C-GENE"]:
 			if row[3] == "IMGT_allele":
 				geneName = row[5]
 			elif row[3] == "pseudo":
 				pseudoList.append(geneName)
 				inVgene = False
-		
+				inJgene = False
+				inCgene = False
 		#check for functional and pseudo V-genes, D-genes, and J-genes, keep track of gene names
 		#If coordinates are "complement", the strand should be "-"
 		#Subtract one from start and end coordinates
@@ -100,24 +104,28 @@ for x in soup.select('div',attrs={'class':'features'}):
 				rows.append([IMGTrefName, str(start - 1), str(end), geneName + " RS", "0", strand])
 		
 		#Check for C genes, and their CDS, keep track of gene names
-		elif inGeneC and geneType == "C":
-				if row[3] == "pseudo" or row[1] == "3'UTR":
-					inGeneC = False
-				elif row[3] == "IMGT_allele":
-					geneName = row[5]
-					if geneName in geneNameList:
-						inGeneC = False
-					else:
-						geneNameList.append(geneName)
-						Cgene = geneName.replace("IGH","IGHC")
-						if re.match("complement",splitStartEnd):
-							strand = "-"
-							splitStartEnd = re.sub(r"complement|\(|\)", "", splitStartEnd)
-						start, end = map(int, splitStartEnd.split(".."))
-						rows.append([IMGTrefName, str(start - 1), str(end), Cgene + " gene", "0", strand])
+		#filter out pseudo C genes
+		elif inGeneCunit and geneType == "C":
+			if geneName in pseudoList or row[1] == "3'UTR":
+				inGeneCunit = False
+			elif row[3] == "IMGT_allele":
+				geneName = row[5]
+				if geneName in geneNameList:
+					inGeneCunit = False
+				else:
+					geneNameList.append(geneName)
+					Cgene = geneName.replace("IGH","IGHC")
+					if re.match("complement",splitStartEnd):
+						strand = "-"
+						splitStartEnd = re.sub(r"complement|\(|\)", "", splitStartEnd)
+					start, end = map(int, splitStartEnd.split(".."))
+					rows.append([IMGTrefName, str(start - 1), str(end), Cgene + " gene", "0", strand])
 				#fetch C-gene CDS
-				if re.match("CL",row[1]) or re.match("CH([1234S])",row[1]) or re.match("H(?:[12])?(?:-)?(?:CH)?",row[1]) or re.match("M(?:[12])?",row[1]) or re.match("EX([1234])",row[1]):
-					splitStartEnd = row[5]
+			if re.match("CL",row[1]) or re.match("CH|H",row[1]) or re.match("M[12]|M(?!ISC)",row[1]) or re.match("EX([1234])",row[1]):
+				splitStartEnd = row[5]
+				if re.match("CH([1234])-CH([2S])|H-CH([1-4])",row[1]):
+					CexonList.append(geneName)
+				if re.match("CH|H|CL|EX([1234])",row[1]) and geneName not in CexonList or re.match("M[12]|M(?!ISC)",row[1]) or re.match("CH([1234])-CH([2S])|H-CH([1-4])",row[1]):
 					if re.match("complement",splitStartEnd):
 						strand = "-"
 						splitStartEnd = re.sub(r"complement|\(|\)", "", splitStartEnd)
@@ -139,8 +147,14 @@ for x in soup.select('div',attrs={'class':'features'}):
 			if re.match("V-GENE",row[1]):
 				inVgene = True
 				geneType = "V-GENE"
+			if re.match("J-GENE",row[1]):
+				inJgene = True
+				geneType = "J-GENE"
+			if re.match("C-GENE",row[1]):
+				inCgene = True
+				geneType = "C-GENE"
 			if row[1] == "C-GENE-UNIT":
-				inGeneC = True
+				inGeneCunit = True
 				splitStartEnd = row[5]
 				geneType = "C"
 
